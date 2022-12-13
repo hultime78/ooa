8a3b3e8de78ebfc59712895667918a6960c8d845
Aaron <aaronmangbenza@gmail.com>
2022-12-13 03:37:40 +0100

    Coded add new Student front-end page

--- src/main/java/org/hlab/bookapirest/controller/StudentController.java
+++ src/main/java/org/hlab/bookapirest/controller/StudentController.java
@@ -1,10 +1,13 @@
 package org.hlab.bookapirest.controller;
 
+import org.hlab.bookapirest.model.Student;
 import org.hlab.bookapirest.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,9 +17,21 @@ public class StudentController {
     @Autowired
     StudentService studentService;
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(Model model){
         model.addAttribute("listStudent",studentService.getStudents());
         return "index";
+    }
+
+    @GetMapping("/addNewStudent")
+    public String newStudentForm(Model model){
+        Student student=new Student();
+        model.addAttribute("student",student);
+        return "newStudent";
+    }
+    @PostMapping("/saveStudent")
+    public String saveStudent(@ModelAttribute Student student){
+        studentService.saveStudent(student);
+        return "redirect:/";
     }
 }
